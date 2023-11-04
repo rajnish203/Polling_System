1686490436188c3847a5d99d1f8f3b974eb5b369
rajnish203 <rajnishdivakar618@gmail.com>
2023-11-04 16:54:10 +0530

    new file added

---
+++ config/mongoose.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+require('dotenv').config()
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,"Error Connecting Database"));
+db.once('open',function(){
+    console.log("Successfully Connected to Database!!");
+});
+
+module.exports = db;
